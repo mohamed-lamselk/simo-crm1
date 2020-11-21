5ec80a6ec4ec59de885e507a4c8c729c35708637
mohamed lamselk <mohamed.lamselk@gmail.com>
2020-11-21 14:23:38 +0100

    .

--- crm1/settings.py
+++ crm1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cz=&%f*9(d*zo$_55p=(p)(eki#p$pb^0159-)8k^6$9c3l&_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["simo-crm1.herokuapp.com", "127.0.0.1"]
 
@@ -84,12 +84,14 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        '''
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "demo_1",
         "USER": "mohamed_lamselk",
         "PASSWORD": "Tawba2015",
         "HOST": "database-1.cb10nv8uehut.us-east-2.rds.amazonaws.com",
         "PORT": "5432",
+        '''
     }
 }
 import dj_database_url
